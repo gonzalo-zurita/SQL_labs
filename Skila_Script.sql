ec68446ca15e9fb93144667cc8b5b18dca949171
gonzalo-zurita <87758381+gonzalo-zurita@users.noreply.github.com>
2021-08-17 17:43:36 +0200

    Add files via upload

---
+++ Skila_Script.sql
@@ -0,0 +1,16 @@
+SELECT * FROM sakila.film;
+
+SELECT title
+FROM sakila.film;
+
+SELECT name AS language
+FROM sakila.language;
+
+SELECT * from sakila.store;
+
+SELECT * from sakila.staff;
+
+SELECT first_name from sakila.staff;
+
+
+
